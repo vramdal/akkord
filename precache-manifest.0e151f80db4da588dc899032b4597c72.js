bcbda801db46cafb083e868f1e446ff24e8c6d9d
Vidar Ramdal <vramdal@gmail.com>
2023-11-26 15:25:58 +0100

    Updates

---
+++ precache-manifest.0e151f80db4da588dc899032b4597c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e769140d55807e573fdec557f8b20a",
+    "url": "/akkord/index.html"
+  },
+  {
+    "revision": "7ff45e4f6e94534b1482",
+    "url": "/akkord/static/css/main.d7a34deb.chunk.css"
+  },
+  {
+    "revision": "cb1312ce60e15bf64ca5",
+    "url": "/akkord/static/js/2.8cba5537.chunk.js"
+  },
+  {
+    "revision": "7ff45e4f6e94534b1482",
+    "url": "/akkord/static/js/main.2a5bfe79.chunk.js"
+  },
+  {
+    "revision": "2e0f4b2df28ebdcdaf96",
+    "url": "/akkord/static/js/runtime-main.5ea0c9e3.js"
+  },
+  {
+    "revision": "b0e1bcc7add6e4443c7078e57438b80a",
+    "url": "/akkord/static/media/musical-notes.b0e1bcc7.svg"
+  }
+]);
